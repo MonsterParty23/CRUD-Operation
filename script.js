5f18244de60f525f7897ed236fb213544de89323
MonsteParty23 <lorelyn.paga@gmail.com>
2025-06-07 16:30:06 +0800

    initial commit

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function onFormSubmit(e){
     if(selectedRow == null) {
         insertNewRecord(formData);
     }else{
-
+        updateRecord(formData);
     }
+    resetForm();
 }
 
 //Retrieve the data
@@ -32,5 +33,40 @@ function insertNewRecord(data) {
     var cell4 = newRow.insertCell(3);
         cell4.innerHTML = data.perPrice;
     var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = `<button>Edit</button> <button>Delete</button>`;
+        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onclick='onDelete(this)'>Delete</button>`;
+}
+
+//Edit the data
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+}
+
+//Update the data
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.productCode;
+    selectedRow.cells[1].innerHTML = formData.product;
+    selectedRow.cells[2].innerHTML = formData.qty;
+    selectedRow.cells[3].innerHTML = formData.perPrice;
+}
+
+//Delete the data
+function onDelete(td) {
+    if(confirm('Are you sure you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("storelist").deleteRow(row.rowIndex);
+    }
+    resetForm();
+}
+
+//Reset the Data
+function resetForm() {
+    document.getElementById("productCode").value = '';
+    document.getElementById("product").value = '';
+    document.getElementById("qty").value = '';
+    document.getElementById("perPrice").value = '';
+    selectedRow = null;
 }
