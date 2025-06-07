3ed1ee5eb868237d17156acbe0bae50aa039ea1d
MonsteParty23 <lorelyn.paga@gmail.com>
2025-06-07 16:18:38 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var selectedRow = null;
+function onFormSubmit(e){
+    event.preventDefault();
+    var formData = readFormData();
+    if(selectedRow == null) {
+        insertNewRecord(formData);
+    }else{
+
+    }
+}
+
+//Retrieve the data
+function readFormData() {
+    var formData = {};
+    formData["productCode"] = document.getElementById("productCode").value;
+    formData["product"] = document.getElementById("product").value;
+    formData["qty"] = document.getElementById("qty").value;
+    formData["perPrice"] = document.getElementById("perPrice").value;
+    return formData;
+}
+
+//Insert the data
+function insertNewRecord(data) {
+    var table = document.getElementById("storelist").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length); 
+    var cell1 = newRow.insertCell(0);
+        cell1.innerHTML = data.productCode;
+    var cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.product;
+    var cell3 = newRow.insertCell(2);
+        cell3.innerHTML = data.qty;
+    var cell4 = newRow.insertCell(3);
+        cell4.innerHTML = data.perPrice;
+    var cell5 = newRow.insertCell(4);
+        cell5.innerHTML = `<button>Edit</button> <button>Delete</button>`;
+}
